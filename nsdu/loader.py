08d55f9aba44e84c2ac6031ea64aa0bf24dcdb3e
watarinishin <watarichanno@gmail.com>
2021-08-02 22:46:35 +0700

    Refactor

--- nsdu/loader.py
+++ nsdu/loader.py
@@ -7,67 +7,54 @@ import pathlib
 
 import pluggy
 
+from nsdu import exceptions
 from nsdu import info
 from nsdu import loader_api
 from nsdu import utils
 
 
-class Loader():
-    """Handling loader plugins.
+class LoaderHandle():
+    """Handle for loaded loaders.
 
     Args:
         proj_name (str): Pluggy project name for this plugin type
-        name (str|list): Name(s) of loader's source file(s)
         loader_config (dict): Loaders' configuration
     """
 
-    def __init__(self, proj_name, name, loader_config):
+    def __init__(self, proj_name, loader_config):
         self.manager = pluggy.PluginManager(proj_name)
         self.manager.add_hookspecs(loader_api)
         self.loader_config = loader_config
-        self.name = name
 
-    def load_a_loader(self, name):
-        """Load a loader's module and register it.
+    def load_loader(self, module):
+        """Load source of loader.
 
         Args:
-            name (str): Name of loader's source file
+            module: Module that contains the source
         """
 
-        path = (info.LOADER_DIR_PATH / name).with_suffix('.py')
-        module = utils.load_module(path, name)
         self.manager.register(module)
 
-    def load_loader(self):
-        """Load loader(s).
-        """
 
-        if isinstance(self.name, list):
-            for name in self.name:
-                self.load_a_loader(name)
-        else:
-            self.load_a_loader(self.name)
-
-
-class PersistentLoader(Loader):
-    """Handling plugins that maintain state for the entire duration of the app.
+class PersistentLoaderHandle(LoaderHandle):
+    """Handle for loaders that maintain state for the entire duration of the app.
 
     Args:
         Same as Loader class
     """
 
-    def __init__(self, proj_name, name, loader_config):
-        super().__init__(proj_name, name, loader_config)
+    def __init__(self, proj_name, loader_config):
+        super().__init__(proj_name, loader_config)
         # Loader instance to reuse the same database connection
         # across hook calls.
         self._loader = None
 
-    def load_loader(self):
+    def load_loader(self, module):
         """Load a loader and return its instance for
         reusing file handlers/database connections.
         """
 
-        super().load_loader()
+        super().load_loader(module)
         self.init_loader()
 
     def init_loader(self):
@@ -78,15 +65,12 @@ class PersistentLoader(Loader):
 
 
 # pylint: disable=maybe-no-member
-class VarLoader(Loader):
-    """Load variables from multiple loaders.
-
-    Args:
-        names (list): Names of loaders' source file
+class VarLoaderHandle(LoaderHandle):
+    """Handle for variable loaders.
     """
 
-    def __init__(self, names, loader_config):
-        super().__init__(info.VAR_LOADER_PROJ, names, loader_config)
+    def __init__(self, loader_config):
+        super().__init__(info.VAR_LOADER_PROJ, loader_config)
 
     def get_all_vars(self):
         vars_list = self.manager.hook.get_vars(config=self.loader_config)
@@ -94,15 +78,12 @@ class VarLoader(Loader):
         return merged_vars_dict
 
 
-class DispatchLoader(PersistentLoader):
-    """Load dispatch information and content.
-
-    Args:
-        name (str): Name of loader's source file
+class DispatchLoaderHandle(PersistentLoaderHandle):
+    """Handle for a dispatch loader.
     """
 
-    def __init__(self, name, loader_config):
-        super().__init__(info.DISPATCH_LOADER_PROJ, name, loader_config)
+    def __init__(self, loader_config):
+        super().__init__(info.DISPATCH_LOADER_PROJ, loader_config)
 
     def init_loader(self):
         self._loader = self.manager.hook.init_dispatch_loader(config=self.loader_config)
@@ -122,29 +103,23 @@ class DispatchLoader(PersistentLoader):
                                                  dispatch_id=dispatch_id)
 
 
-class SimpleBBLoader(Loader):
-    """Load simple BBCode formatter config.
-
-    Args:
-        name (str): Name of loader's source file
+class SimpleBBLoaderHandle(LoaderHandle):
+    """Handle for a simple BBCode formatter loader.
     """
 
-    def __init__(self, name, loader_config):
-        super().__init__(info.SIMPLE_BB_LOADER_PROJ, name, loader_config)
+    def __init__(self, loader_config):
+        super().__init__(info.SIMPLE_BB_LOADER_PROJ, loader_config)
 
     def get_simple_bb_config(self):
         return self.manager.hook.get_simple_bb_config(config=self.loader_config)
 
 
-class CredLoader(PersistentLoader):
-    """Load nation login credentials.
-
-    Args:
-        name (str): Name of loader's source file
+class CredLoaderHandle(PersistentLoaderHandle):
+    """Handle for a login credential loader.
     """
 
-    def __init__(self, name, loader_config):
-        super().__init__(info.CRED_LOADER_PROJ, name, loader_config)
+    def __init__(self, loader_config):
+        super().__init__(info.CRED_LOADER_PROJ, loader_config)
 
     def init_loader(self):
         self._loader = self.manager.hook.init_cred_loader(config=self.loader_config)
--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,111 +1,114 @@
 import pathlib
+import importlib
+import mock
 
+import pytest
+
+from nsdu import exceptions
 from nsdu import loader
 from nsdu import info
 
 
-info.LOADER_DIR_PATH = pathlib.Path('tests/resources')
+LOADER_DIR_PATH = pathlib.Path('tests/resources')
 
 
-DISPATCH_LOADER_NAME = 'dispatchloader-test1'
+DISPATCH_LOADER_NAME = 'dispatchloader-test1.py'
 DISPATCH_LOADER_CONFIG = {'dispatchloader-test1': {'key1': 'val1'}}
 
 
-class TestDispatchLoader():
+class TestDispatchLoaderHandle():
     def test_get_dispatch_config(self):
-        obj = loader.DispatchLoader(DISPATCH_LOADER_NAME,
-                                    DISPATCH_LOADER_CONFIG)
-        obj.load_loader()
+        handle = loader.DispatchLoaderHandle(DISPATCH_LOADER_CONFIG)
+        handle.load_loader(load_module(LOADER_DIR_PATH / DISPATCH_LOADER_NAME))
 
-        r = obj.get_dispatch_config()
+        r = handle.get_dispatch_config()
 
-        obj.cleanup_loader()
+        handle.cleanup_loader()
 
         assert r == {'foo1': 'bar1', 'foo2': 'bar2'}
 
     def test_get_dispatch_text(self):
-        obj = loader.DispatchLoader(DISPATCH_LOADER_NAME,
-                                    DISPATCH_LOADER_CONFIG)
-        obj.load_loader()
+        handle = loader.DispatchLoaderHandle(DISPATCH_LOADER_CONFIG)
+        handle.load_loader(load_module(LOADER_DIR_PATH / DISPATCH_LOADER_NAME))
 
-        r = obj.get_dispatch_text('test')
+        r = handle.get_dispatch_text('test')
 
-        obj.cleanup_loader()
+        handle.cleanup_loader()
 
         assert r == 'Dispatch content of test'
 
     def test_add_dispatch_id(self):
-        obj = loader.DispatchLoader(DISPATCH_LOADER_NAME,
-                                    DISPATCH_LOADER_CONFIG)
-        obj.load_loader()
+        handle = loader.DispatchLoaderHandle(DISPATCH_LOADER_CONFIG)
+        handle.load_loader(load_module(LOADER_DIR_PATH / DISPATCH_LOADER_NAME))
 
-        r = obj.add_dispatch_id('test', '123456')
+        r = handle.add_dispatch_id('test', '123456')
 
-        obj.cleanup_loader()
+        handle.cleanup_loader()
 
         assert r
 
 
-VAR_LOADER_NAMES = ['varloader-test1', 'varloader-test2']
+VAR_LOADER_NAMES = ['varloader-test1.py', 'varloader-test2.py']
 VAR_LOADER_CONFIG = {'varloader-test1': {'key1': 'val1'},
                      'varloader-test2': {'key2': 'val2'}}
 
 
-class TestVarLoader():
+class TestVarLoaderHandle():
     def test_get_all_vars(self):
-        obj = loader.VarLoader(VAR_LOADER_NAMES, VAR_LOADER_CONFIG)
-        obj.load_loader()
+        handle = loader.VarLoaderHandle(VAR_LOADER_CONFIG)
+        for name in VAR_LOADER_NAMES:
+            handle.load_loader(load_module(LOADER_DIR_PATH / name))
 
-        r = obj.get_all_vars()
+        r = handle.get_all_vars()
 
         assert r == {'key1': {'key1': 'val1'}, 'key2': {'key2': 'val2'}}
 
 
-SIMPLE_BB_LOADER_NAME = 'simplebbloader-test1'
+SIMPLE_BB_LOADER_NAME = 'simplebbloader-test1.py'
 SIMPLE_BB_LOADER_CONFIG = {'simplebbloader-test1': {'key1': 'val1'}}
 
 
-class TestSimpleBBLoader():
+class TestSimpleBBLoaderHandle():
     def test_get_simple_bb_config(self):
-        obj = loader.SimpleBBLoader(SIMPLE_BB_LOADER_NAME, SIMPLE_BB_LOADER_CONFIG)
-        obj.load_loader()
+        handle = loader.SimpleBBLoaderHandle(SIMPLE_BB_LOADER_CONFIG)
+        handle.load_loader(load_module(LOADER_DIR_PATH / SIMPLE_BB_LOADER_NAME))
 
-        r = obj.get_simple_bb_config()
+        r = handle.get_simple_bb_config()
 
         assert r == {'key1': {'key1': 'val1'}}
 
 
-CRED_LOADER_NAME = 'credloader-test1'
+CRED_LOADER_NAME = 'credloader-test1.py'
 CRED_LOADER_CONFIG = {'credloader-test1': {'key1': 'val1'}}
 
 
-class TestCredLoader():
+class TestCredLoaderHandle():
     def test_get_all_creds(self):
-        obj = loader.CredLoader(CRED_LOADER_NAME, CRED_LOADER_CONFIG)
-        obj.load_loader()
+        handle = loader.CredLoaderHandle(CRED_LOADER_CONFIG)
+        handle.load_loader(load_module(LOADER_DIR_PATH / CRED_LOADER_NAME))
 
-        r = obj.get_creds()
+        r = handle.get_creds()
 
-        obj.cleanup_loader()
+        handle.cleanup_loader()
 
         assert r == {'nation1': '123456'}
 
     def test_add_cred(self):
-        obj = loader.CredLoader(CRED_LOADER_NAME, CRED_LOADER_CONFIG)
-        obj.load_loader()
+        handle = loader.CredLoaderHandle(CRED_LOADER_CONFIG)
+        handle.load_loader(load_module(LOADER_DIR_PATH / CRED_LOADER_NAME))
 
-        r = obj.add_cred('nation1', '123456')
+        r = handle.add_cred('nation1', '123456')
 
-        obj.cleanup_loader()
+        handle.cleanup_loader()
 
         assert r
 
     def test_remove_cred(self):
-        obj = loader.CredLoader(CRED_LOADER_NAME, CRED_LOADER_CONFIG)
-        obj.load_loader()
+        handle = loader.CredLoaderHandle(CRED_LOADER_CONFIG)
+        handle.load_loader(load_module(LOADER_DIR_PATH / CRED_LOADER_NAME))
 
-        r = obj.remove_cred('nation1')
+        r = handle.remove_cred('nation1')
 
-        obj.cleanup_loader()
+        handle.cleanup_loader()
 
         assert r
