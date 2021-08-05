4b3a0cd019c8811694e2f5c54b8470c78323a9bb
watarinishin <watarichanno@gmail.com>
2021-08-06 01:23:02 +0700

    Refactor

--- nsdu/__main__.py
+++ nsdu/__main__.py
@@ -10,137 +10,98 @@ from nsdu import info
 from nsdu import exceptions
 from nsdu import api_adapter
 from nsdu import loader
-from nsdu import renderer
-from nsdu import updater
+from nsdu import updater_api
 from nsdu import utils
 
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('NSDU')
 
 
 class NsduDispatch():
-    """NSDU dispatch update utility.
+    def __init__(self, dispatch_updater, dispatch_loader_manager, dispatch_config, dispatch_info, creds):
+        self.dispatch_updater = dispatch_updater
+        self.dispatch_loader_manager = dispatch_loader_manager
+        self.dispatch_config = dispatch_config
+        self.dispatch_info = dispatch_info
+        self.creds = creds
 
-    Args:
-        config (dict): General configuration
-    """
+    def update_a_dispatch(self, name):
+        config = self.dispatch_info[name]
+        category = config['category']
+        subcategory = config['subcategory']
+        title = config['title']
+        action = config['action']
 
-    def __init__(self, config):
-        self.config = config
+        if action not in ('create', 'edit', 'remove'):
+            raise exceptions.DispatchConfigError('Invalid action "{}" on dispatch "{}".'.format(action, name))
 
-        dispatch_api = api_adapter.DispatchAPI(self.config['general']['user_agent'])
-
-        self.custom_loader_dir_path = self.config['general'].get('custom_loader_dir_path', None)
-        loader_config = self.config['loader_config']
-
-        self.dispatch_loader_manager = loader.DispatchLoaderManager(loader_config)
-        self.var_loader_manager = loader.VarLoaderManager(loader_config)
-        self.simple_bb_loader_manager = loader.SimpleBBLoaderManager(loader_config)
-        self.cred_loader_manager = loader.CredLoaderManager(loader_config)
-
-        plugin_opt = self.config['plugins']
         try:
-            entry_points = import_metadata.entry_points()[info.LOADER_ENTRY_POINT_NAME]
-        except KeyError:
-            entry_points = []
-        singleloader_builder = loader.SingleLoaderManagerBuilder(info.LOADER_DIR_PATH,
-                                                                self.custom_loader_dir_path,
-                                                                entry_points)
-        multiloaders_builder = loader.MultiLoadersManagerBuilder(info.LOADER_DIR_PATH,
-                                                                self.custom_loader_dir_path,
-                                                                entry_points)
+            if action == 'create':
+                logger.debug('Creating dispatch "%s" with params: %r', name, config)
+                new_dispatch_id = self.dispatch_updater.create_dispatch(name, title, category, subcategory)
+                logger.debug('Got id "%s" of new dispatch "%s".', new_dispatch_id, name)
+                self.dispatch_loader_manager.add_dispatch_id(name, new_dispatch_id)
+                logger.info('Created dispatch "%s".', name)
+            elif action == 'edit':
+                dispatch_id = config['ns_id']
+                logger.debug('Editing dispatch "%s" with id "%s" and with params: %r',
+                             name, dispatch_id, config)
+                self.dispatch_updater.edit_dispatch(name, dispatch_id, title, category, subcategory)
+                logger.info('Edited dispatch "%s".', name)
+            elif action == 'remove':
+                dispatch_id = config['ns_id']
+                logger.debug('Removing dispatch "%s" with id "%s".', name, dispatch_id)
+                self.dispatch_updater.remove_dispatch(dispatch_id)
+                logger.info('Removed dispatch "%s".', name)
+        except exceptions.UnknownDispatchError:
+            logger.error('Could not find dispatch "%s" with id "%s".', name, dispatch_id)
+        except exceptions.NotOwnerDispatchError:
+            logger.error('Dispatch "%s" is not owned by this nation.', name)
+        except exceptions.NonexistentCategoryError as err:
+            logger.error('%s "%s" of dispatch "%s" is invalid.', err.category_type, err.category_value, name)
 
-        singleloader_builder.load_loader(self.cred_loader_manager, plugin_opt['cred_loader'])
-        self.creds = utils.CredManager(self.cred_loader_manager, dispatch_api)
-        self.creds.load_creds()
-
-        singleloader_builder.load_loader(self.dispatch_loader_manager, plugin_opt['dispatch_loader'])
-        self.dispatch_config = self.dispatch_loader_manager.get_dispatch_config()
-
-        singleloader_builder.load_loader(self.simple_bb_loader_manager, plugin_opt['simple_bb_loader'])
-        simple_bb_config = self.simple_bb_loader_manager.get_simple_bb_config()
-
-        multiloaders_builder.load_loader(self.var_loader_manager, plugin_opt['var_loader'])
-        template_vars = self.var_loader_manager.get_all_vars()
-        self.dispatch_info = utils.get_dispatch_info(self.dispatch_config)
-        template_vars['dispatch_info'] = self.dispatch_info
-
-        rendering_config = self.config.get('rendering', {})
-        self.renderer = renderer.DispatchRenderer(self.dispatch_loader_manager.get_dispatch_template, simple_bb_config,
-                                                  rendering_config.get('complex_formatter_source_path', None),
-                                                  rendering_config.get('filter_paths', None), template_vars)
-
-        self.updater = updater.DispatchUpdater(dispatch_api, self.creds,
-                                               self.renderer, self.dispatch_loader_manager)
-
-    def update_dispatches(self, dispatches):
+    def update_dispatches(self, names):
         """Update dispatches. Empty list means update all.
 
         Args:
-            dispatches (list): Dispatch names
+            names (list): Dispatch names
         """
 
-        if dispatches:
-            while (dispatches[-1] not in self.dispatch_info):
-                logger.error('Could not find dispatch "%s"', dispatches[-1])
-                dispatches.pop()
-                if not dispatches:
+        if names:
+            while (names[-1] not in self.dispatch_info):
+                logger.error('Could not find dispatch "%s"', names[-1])
+                names.pop()
+                if not names:
                     return
 
         for owner_nation, dispatch_config in self.dispatch_config.items():
             try:
-                self.updater.login_owner_nation(owner_nation, dispatch_config)
+                self.dispatch_updater.login_owner_nation(owner_nation, autologin=self.creds[owner_nation])
                 logger.info('Logged in nation "%s".', owner_nation)
             except exceptions.NationLoginError:
                 logger.error('Could not log into nation "%s".', owner_nation)
                 continue
 
-            if not dispatches:
-                [self.updater.update_dispatch(name) for name in dispatch_config.keys()]
+            if not names:
+                [self.update_a_dispatch(name) for name in dispatch_config.keys()]
             else:
-                [self.updater.update_dispatch(name) for name in dispatch_config.keys() if name in dispatches]
-
-    def add_nation_cred(self, nation_name, password):
-        """Add new credentials.
-
-        Args:
-            nation_name (str): Nation name
-            password (str): Password
-        """
-
-        self.creds[nation_name] = password
-
-    def remove_nation_cred(self, nation_name):
-        """Remove credentials.
-
-        Args:
-            nation_name (str): Nation name
-        """
-
-        del self.creds[nation_name]
+                [self.update_a_dispatch(name) for name in dispatch_config.keys() if name in names]
 
     def close(self):
-        """Cleanup.
-        """
-
         self.dispatch_loader_manager.cleanup_loader()
-        self.creds.save()
-        self.cred_loader_manager.cleanup_loader()
 
 
 class NsduCred():
     """NSDU credential management utility.
 
     Args:
-        config (dict): General configuration
-     """
+        dispatch_api (nsdu.api_adapter.DispatchApi): Dispatch API
+        cred_loader_manager (nsdu.loader.CredLoaderManager): Cred loader manager
+    """
 
-    def __init__(self, config):
-        dispatch_api = api_adapter.DispatchAPI(config['general']['user_agent'])
-
-        self.cred_loader_manager = loader.CredLoaderManager(config['loader_config'])
-        self.creds = utils.CredManager(self.cred_loader_manager, dispatch_api)
-        self.creds.load_creds()
+    def __init__(self, cred_loader_manager, dispatch_api):
+        self.dispatch_api = dispatch_api
+        self.cred_loader_manager = cred_loader_manager
 
     def add_nation_cred(self, nation_name, password):
         """Add new credentials.
@@ -150,7 +111,8 @@ class NsduCred():
             password (str): Password
         """
 
-        self.creds[nation_name] = password
+        x_autologin = self.dispatch_api.login(nation_name, password=password)
+        self.cred_loader_manager.add_cred(nation_name, x_autologin)
 
     def remove_nation_cred(self, nation_name):
         """Remove credentials.
@@ -159,14 +121,65 @@ class NsduCred():
             nation_name (str): Nation name
         """
 
-        del self.creds[nation_name]
+        self.cred_loader_manager.remove_cred(nation_name)
 
     def close(self):
         """Save changes to creds and close.
         """
 
-        self.creds.save()
         self.cred_loader_manager.cleanup_loader()
+
+
+def load_nsdu_dispatch_utility_from_config(config):
+    custom_loader_dir_path = config['general'].get('custom_loader_dir_path', None)
+    loader_config = config['loader_config']
+
+    dispatch_loader_manager = loader.DispatchLoaderManager(loader_config)
+    var_loader_manager = loader.VarLoaderManager(loader_config)
+    simple_bb_loader_manager = loader.SimpleBBLoaderManager(loader_config)
+    cred_loader_manager = loader.CredLoaderManager(loader_config)
+
+    plugin_opt = config['plugins']
+    try:
+        entry_points = import_metadata.entry_points()[info.LOADER_ENTRY_POINT_NAME]
+    except KeyError:
+        entry_points = []
+    singleloader_builder = loader.SingleLoaderManagerBuilder(info.LOADER_DIR_PATH,
+                                                             custom_loader_dir_path,
+                                                             entry_points)
+    multiloaders_builder = loader.MultiLoadersManagerBuilder(info.LOADER_DIR_PATH,
+                                                             custom_loader_dir_path,
+                                                             entry_points)
+
+    singleloader_builder.load_loader(cred_loader_manager, plugin_opt['cred_loader'])
+    creds = cred_loader_manager.get_creds()
+
+    singleloader_builder.load_loader(dispatch_loader_manager, plugin_opt['dispatch_loader'])
+    dispatch_config = dispatch_loader_manager.get_dispatch_config()
+
+    singleloader_builder.load_loader(simple_bb_loader_manager, plugin_opt['simple_bb_loader'])
+    simple_bb_config = simple_bb_loader_manager.get_simple_bb_config()
+
+    multiloaders_builder.load_loader(var_loader_manager, plugin_opt['var_loader'])
+    template_vars = var_loader_manager.get_all_vars()
+    dispatch_info = utils.get_dispatch_info(dispatch_config)
+    template_vars['dispatch_info'] = dispatch_info
+
+    rendering_config = config.get('rendering', {})
+    dispatch_updater = updater_api.DispatchUpdater(user_agent=config['general']['user_agent'],
+                                                   template_filter_paths=rendering_config.get('filter_paths', None),
+                                                   simple_formatter_config=simple_bb_config,
+                                                   complex_formatter_source_path=rendering_config.get('complex_formatter_source_path', None),
+                                                   template_load_func=dispatch_loader_manager.get_dispatch_template,
+                                                   template_vars=template_vars)
+
+    return NsduDispatch(dispatch_updater, dispatch_loader_manager, dispatch_config, dispatch_info, creds)
+
+
+def load_nsdu_cred_utility_from_config(config):
+    dispatch_api = api_adapter.DispatchApi(config['general']['user_agent'])
+    cred_loader_manager = loader.CredLoaderManager(config['loader_config'])
+    return NsduCred(dispatch_api, cred_loader_manager)
 
 
 def run(config, inputs):
@@ -178,7 +191,7 @@ def run(config, inputs):
     """
 
     if inputs.subparser_name == 'cred':
-        app = NsduCred(config)
+        app = load_nsdu_cred_utility_from_config(config)
         if hasattr(inputs, 'add') and inputs.add is not None:
             if len(inputs.add) % 2 != 0:
                 print('There is no password for the last name.')
@@ -189,7 +202,7 @@ def run(config, inputs):
             for nation_name in inputs.remove:
                 app.remove_nation_cred(nation_name)
     elif inputs.subparser_name == 'update':
-        app = NsduDispatch(config)
+        app = load_nsdu_dispatch_utility_from_config(config)
         app.update_dispatches(inputs.dispatches)
 
     app.close()
---
+++ tests/test_nsdu.py
@@ -0,0 +1,202 @@
+from unittest import mock
+
+import pytest
+
+from nsdu import exceptions
+from nsdu import __main__
+
+
+def get_dispatch_info(dispatch_config):
+    """Return dispatch information for use as context in the template renderer.
+
+    Args:
+        dispatch_config (dict): Dispatch configuration.
+        id_store (IDStore): Dispatch ID store.
+
+    Returns:
+        dict: Dispatch information.
+    """
+
+    dispatch_info = {}
+    for nation, dispatches in dispatch_config.items():
+        for name, config in dispatches.items():
+            config['owner_nation'] = nation
+            dispatch_info[name] = config
+
+    return dispatch_info
+
+
+class TestNsduCred():
+    def test_add_nation_cred(self):
+        mock_cred_loader = mock.Mock(add_cred=mock.Mock())
+        mock_dispatch_api = mock.Mock(login=mock.Mock(return_value='123456'))
+        app = __main__.NsduCred(mock_cred_loader, mock_dispatch_api)
+
+        app.add_nation_cred('nation1', '123456')
+        app.close()
+
+        mock_cred_loader.add_cred.assert_called_with('nation1', '123456')
+
+    def test_remove_nation_cred(self):
+        mock_cred_loader = mock.Mock(remove_cred=mock.Mock())
+        app = __main__.NsduCred(mock_cred_loader, mock.Mock())
+
+        app.remove_nation_cred('nation1')
+        app.close()
+
+        mock_cred_loader.remove_cred.assert_called_with('nation1')
+
+
+class TestNsduDispatch():
+    def test_update_a_dispatch_with_create_action(self):
+        dispatch_loader_manager = mock.Mock()
+        dispatch_updater = mock.Mock(create_dispatch=mock.Mock(return_value='12345'))
+        dispatch_info = {'foo': {'action': 'create',
+                                 'title': 'Test title',
+                                 'category': '1',
+                                 'subcategory': '100'}}
+        app = __main__.NsduDispatch(dispatch_updater, dispatch_loader_manager, {}, dispatch_info, {})
+
+        app.update_a_dispatch('foo')
+
+        dispatch_loader_manager.add_dispatch_id.assert_called_with('foo', '12345')
+
+    def test_update_a_dispatch_with_edit_action(self):
+        dispatch_loader_manager = mock.Mock()
+        dispatch_updater = mock.Mock()
+        dispatch_info = {'foo': {'action': 'edit',
+                                 'ns_id': '12345',
+                                 'title': 'Test title',
+                                 'category': '1',
+                                 'subcategory': '100'}}
+        app = __main__.NsduDispatch(dispatch_updater, dispatch_loader_manager, {}, dispatch_info, {})
+
+        app.update_a_dispatch('foo')
+
+        dispatch_updater.edit_dispatch.assert_called_with('foo', '12345', 'Test title', '1', '100')
+
+    def test_update_a_dispatch_with_remove_action(self):
+        dispatch_loader_manager = mock.Mock()
+        dispatch_updater = mock.Mock()
+        dispatch_info = {'foo': {'action': 'remove',
+                                 'ns_id': '12345',
+                                 'title': 'Test title',
+                                 'category': '1',
+                                 'subcategory': '100'}}
+        app = __main__.NsduDispatch(dispatch_updater, dispatch_loader_manager, {}, dispatch_info, {})
+
+        app.update_a_dispatch('foo')
+
+        dispatch_updater.remove_dispatch.assert_called_with('12345')
+
+    def test_update_a_dispatch_with_invalid_action(self):
+        dispatch_loader_manager = mock.Mock()
+        dispatch_updater = mock.Mock()
+        dispatch_info = {'foo': {'action': 'abcd',
+                                 'ns_id': '12345',
+                                 'title': 'Test title',
+                                 'category': '1',
+                                 'subcategory': '100'}}
+        app = __main__.NsduDispatch(dispatch_updater, dispatch_loader_manager, {}, dispatch_info, {})
+
+        with pytest.raises(exceptions.DispatchConfigError):
+            app.update_a_dispatch('foo')
+
+    @pytest.mark.parametrize("api_exceptions", [exceptions.UnknownDispatchError,
+                                                exceptions.NotOwnerDispatchError,
+                                                exceptions.NonexistentCategoryError('','')])
+    def test_update_a_dispatch_with_api_exceptions(self, api_exceptions, caplog):
+        dispatch_loader_manager = mock.Mock()
+        dispatch_updater = mock.Mock(edit_dispatch=mock.Mock(side_effect=api_exceptions))
+        dispatch_info = {'foo': {'action': 'edit',
+                                 'ns_id': '12345',
+                                 'title': 'Test title',
+                                 'category': '1',
+                                 'subcategory': '100'}}
+        app = __main__.NsduDispatch(dispatch_updater, dispatch_loader_manager, {}, dispatch_info, {})
+
+        app.update_a_dispatch('foo')
+
+        assert caplog.records[-1].levelname == 'ERROR'
+
+    def test_update_dispatches_with_all_dispatches_and_existent_dispatches(self):
+        dispatch_loader_manager = mock.Mock()
+        dispatch_updater = mock.Mock()
+        dispatch_config = {'nation1' : {'foo': {'action': 'create',
+                                                'ns_id': '12345',
+                                                'title': 'Test title 1',
+                                                'category': '1',
+                                                'subcategory': '100'},
+                                        'bar': {'action': 'edit',
+                                                'ns_id': '54321',
+                                                'title': 'Test title 2',
+                                                'category': '1',
+                                                'subcategory': '200'}},
+                           'nation2': {'zoo': {'action': 'remove',
+                                               'ns_id': '98765',
+                                               'title': 'Test title 1',
+                                               'category': '1',
+                                               'subcategory': '100'}}}
+        dispatch_info = get_dispatch_info(dispatch_config)
+        creds = {'nation1': 'abcd1234', 'nation2': '4321bcda'}
+        app = __main__.NsduDispatch(dispatch_updater, dispatch_loader_manager, dispatch_config, dispatch_info, creds)
+
+        app.update_dispatches([])
+
+        dispatch_updater.create_dispatch.assert_called()
+        dispatch_updater.edit_dispatch.assert_called()
+        dispatch_updater.remove_dispatch.assert_called()
+
+    def test_update_dispatches_with_some_dispatches_and_existent_dispatches(self):
+        dispatch_loader_manager = mock.Mock()
+        dispatch_updater = mock.Mock()
+        dispatch_config = {'nation1' : {'foo': {'action': 'create',
+                                                'ns_id': '12345',
+                                                'title': 'Test title 1',
+                                                'category': '1',
+                                                'subcategory': '100'},
+                                        'bar': {'action': 'edit',
+                                                'ns_id': '54321',
+                                                'title': 'Test title 2',
+                                                'category': '1',
+                                                'subcategory': '200'}},
+                           'nation2': {'zoo': {'action': 'remove',
+                                               'ns_id': '98765',
+                                               'title': 'Test title 1',
+                                               'category': '1',
+                                               'subcategory': '100'}}}
+        dispatch_info = get_dispatch_info(dispatch_config)
+        creds = {'nation1': 'abcd1234', 'nation2': '4321bcda'}
+        app = __main__.NsduDispatch(dispatch_updater, dispatch_loader_manager, dispatch_config, dispatch_info, creds)
+
+        app.update_dispatches(['foo', 'zoo'])
+
+        dispatch_updater.create_dispatch.assert_called()
+        dispatch_updater.edit_dispatch.assert_not_called()
+        dispatch_updater.remove_dispatch.assert_called()
+
+    def test_update_dispatches_with_all_non_existent_dispatches(self, caplog):
+        dispatch_loader_manager = mock.Mock()
+        dispatch_updater = mock.Mock()
+        dispatch_config = {'nation1' : {'foo': {'action': 'create',
+                                                'ns_id': '12345',
+                                                'title': 'Test title 1',
+                                                'category': '1',
+                                                'subcategory': '100'},
+                                        'bar': {'action': 'edit',
+                                                'ns_id': '54321',
+                                                'title': 'Test title 2',
+                                                'category': '1',
+                                                'subcategory': '200'}},
+                           'nation2': {'zoo': {'action': 'remove',
+                                               'ns_id': '98765',
+                                               'title': 'Test title 1',
+                                               'category': '1',
+                                               'subcategory': '100'}}}
+        dispatch_info = get_dispatch_info(dispatch_config)
+        creds = {'nation1': 'abcd1234', 'nation2': '4321bcda'}
+        app = __main__.NsduDispatch(dispatch_updater, dispatch_loader_manager, dispatch_config, dispatch_info, creds)
+
+        app.update_dispatches(['voo', 'doo'])
+
+        assert caplog.records[-1].levelname == 'ERROR'
