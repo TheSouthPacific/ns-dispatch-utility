72c49ebbacaf7edb8a2cce1069dcee1cbfbac6a6
watarichanno <watarichanno@gmail.com>
2023-09-20 23:35:52 +0700

    Refactor

--- nsdu/updater_api.py
+++ nsdu/updater_api.py
@@ -1,9 +1,10 @@
-"""Updates dispatches based on dispatch config from dispatch loader.
+"""A simple API to render dispatches from templates
+and upload them to NationStates.
 """
 
 import logging
 from pathlib import Path
-from typing import Sequence, Tuple
+from typing import Sequence
 
 from nsdu import exceptions, info, ns_api, renderer
 from nsdu.bbc_parser import SimpleFormattersConfig
@@ -13,54 +14,43 @@ logger = logging.getLogger(__name__)
 
 
 class DispatchMetadataError(exceptions.NSDUError):
-    """Dispatch config error."""
+    """Dispatch metadata error."""
 
 
-class NonExistentCategoryError(DispatchMetadataError):
-    """Category or subcategory doesn't exist."""
-
-    def __init__(self, category_type, category_value):
-        self.category_type = category_type
-        self.category_value = category_value
-        super().__init__()
-
-
-def get_category_number(category: str, subcategory: str) -> Tuple[str, str]:
-    """Get the number of a dispatch category or subcategory name.
-    If the provided names are numbers, just return the numbers as is.
+def get_category_numbers(category: str, subcategory: str) -> tuple[str, str]:
+    """Get category and subcategory number from their names.
+    If the provided names are numbers, return the numbers as is.
 
     Args:
-        category (str): Category name or number
-        subcategory (str): Subcategory name or number
+        category (str): Category name
+        subcategory (str): Subcategory name
 
     Raises:
-        exceptions.DispatchUpdatingError: Could not find (sub)category number
-        from provided name
+        DispatchMetadataError: Invalid name
 
     Returns:
-        Tuple[str, str]: Category and subcategory number
+        tuple[str, str]: Category, subcategory number
     """
 
-    if category.isalpha() and subcategory.isalpha():
-        try:
-            category_info = info.CATEGORIES[category.lower()]
-            category_num = category_info["num"]
-        except KeyError as err:
-            raise NonExistentCategoryError("category", category) from err
+    if category.isnumeric() and subcategory.isnumeric():
+        return category, subcategory
 
-        try:
-            subcategory_num = category_info["subcategories"][subcategory.lower()]
-        except KeyError as err:
-            raise NonExistentCategoryError("subcategory", subcategory) from err
-    else:
-        category_num = category
-        subcategory_num = subcategory
+    try:
+        category_info = info.CATEGORIES[category.lower()]
+        category_num = category_info["num"]
+    except KeyError as err:
+        raise DispatchMetadataError(f"Category {category} is invalid") from err
+
+    try:
+        subcategory_num = category_info["subcategories"][subcategory.lower()]
+    except KeyError as err:
+        raise DispatchMetadataError(f"Subcategory {subcategory} is invalid") from err
 
     return category_num, subcategory_num
 
 
 class DispatchUpdater:
-    """Render dispatches from templates and uploads them to NationStates."""
+    """Render dispatches from templates and upload them to NationStates."""
 
     def __init__(
         self,
@@ -71,19 +61,17 @@ class DispatchUpdater:
         template_load_func: TemplateLoadFunc,
         template_vars: TemplateVars,
     ) -> None:
-        """Renders dispatches from templates and uploads them to NationStates.
+        """Renders and uploads dispatches to NationStates.
 
         Args:
-            user_agent (str): User agent for NationStates API calls
-            template_filter_paths (Sequence[str]): List of paths to template filter
-            source files
-            simple_formatter_config (SimpleFormattersConfig | None): Simple BBCode
-            formatter config
-            complex_formatter_source_path (Path | None): Path to complex BBCode
-            formatter
-            source file
-            template_load_func (TemplateLoadFunc): A callable that receives a
-            dispatch name and returns its template
+            user_agent (str): User agent for NationStates API
+            template_filter_paths (Sequence[str]): Paths to template filter source files
+            simple_formatter_config (SimpleFormattersConfig | None): Config for
+            simple BBCode formatters
+            complex_formatter_source_path (Path | None): Path to source file of
+            complex BBCode formatters
+            template_load_func (TemplateLoadFunc): A callback which receives
+            dispatch name and returns template text
             template_vars (TemplateVars): Template variables
         """
 
@@ -96,24 +84,24 @@ class DispatchUpdater:
             template_vars,
         )
 
-    def set_owner_nation(self, nation_name: str, autologin: str) -> None:
+    def set_nation(self, nation_name: str, autologin: str) -> None:
         """Set the nation to do dispatch operations on.
 
         Args:
             nation_name (str): Nation name
-            autologin (str): Nation's autologin code
+            autologin (str): Autologin code
         """
 
-        self.dispatch_api.set_owner_nation(nation_name, autologin)
+        self.dispatch_api.set_nation(nation_name, autologin)
 
-    def get_rendered_dispatch_text(self, name: str) -> str:
-        """Get rendered text of a dispatch.
+    def render_dispatch(self, name: str) -> str:
+        """Render a dispatch.
 
         Args:
             name (str): Dispatch name
 
         Returns:
-            str: Rendered text
+            str: Text of rendered dispatch
         """
 
         return self.renderer.render(name)
@@ -130,18 +118,16 @@ class DispatchUpdater:
             subcategory (str): Subcategory name or number
 
         Returns:
-            str: Id of new dispatch
+            str: ID of new dispatch
         """
 
-        logger.info('Creating dispatch "%s".', name)
-        text = self.get_rendered_dispatch_text(name)
-        category_num, subcategory_num = get_category_number(category, subcategory)
+        text = self.render_dispatch(name)
+        category_num, subcategory_num = get_category_numbers(category, subcategory)
 
         new_dispatch_id = self.dispatch_api.create_dispatch(
             title=title, text=text, category=category_num, subcategory=subcategory_num
         )
-        logger.debug('Got ID "%s" of new dispatch "%s".', new_dispatch_id, name)
-        logger.debug('Created dispatch "%s"', name)
+
         return new_dispatch_id
 
     def edit_dispatch(
@@ -151,16 +137,14 @@ class DispatchUpdater:
 
         Args:
             name (str): Name
-            dispatch_id (str): Id
+            dispatch_id (str): ID
             title (str): Title
             category (str): Category name or number
             subcategory (str): Subcategory name or number
         """
 
-        logger.info('Editing dispatch "%s".', name)
-
-        text = self.get_rendered_dispatch_text(name)
-        category_num, subcategory_num = get_category_number(category, subcategory)
+        text = self.render_dispatch(name)
+        category_num, subcategory_num = get_category_numbers(category, subcategory)
 
         self.dispatch_api.edit_dispatch(
             dispatch_id=dispatch_id,
@@ -169,15 +153,12 @@ class DispatchUpdater:
             category=category_num,
             subcategory=subcategory_num,
         )
-        logger.debug('Edited dispatch "%s"', name)
 
-    def remove_dispatch(self, name: str, dispatch_id: str) -> None:
+    def delete_dispatch(self, dispatch_id: str) -> None:
         """Delete a dispatch.
 
         Args:
-            dispatch_id (str):  Id
+            dispatch_id (str): ID
         """
 
-        logger.info('Deleting dispatch "%s".', name)
-        self.dispatch_api.remove_dispatch(dispatch_id)
-        logger.debug('Deleted dispatch "%s".', name)
+        self.dispatch_api.delete_dispatch(dispatch_id)
