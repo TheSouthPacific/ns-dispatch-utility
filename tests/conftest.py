a957f14ec7450dd2a0f1f0d4273ec8844b6eacbf
watarichanno <watarichanno@gmail.com>
2023-09-25 00:52:31 +0700

    Use generic test file generation function

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
+import functools
 import json
 from pathlib import Path
-from typing import Mapping, NamedTuple
+from typing import Callable, Mapping, NamedTuple, TypeVar
 
 import pytest
 import toml
@@ -11,63 +12,61 @@ class Paths(NamedTuple):
     file_paths: list[Path]
 
 
+T = TypeVar("T")
+
+
+def gen_files(
+    gen_func: Callable[[Path, T], None],
+    tmp_path: Path,
+    default_files: Mapping[str, T],
+    files: Mapping[str, T],
+) -> Paths:
+    if files is None:
+        files = default_files
+
+    file_paths = []
+
+    for name, content in files.items():
+        file_path = tmp_path / name
+        gen_func(file_path, content)
+        file_paths.append(file_path)
+
+    return Paths(tmp_path, file_paths)
+
+
+def gen_toml_file(file_path: Path, content: dict):
+    with open(file_path, "w") as file:
+        toml.dump(content, file)
+
+
 @pytest.fixture
 def toml_files(tmp_path):
     """Generate TOML files for testing."""
 
-    def gen_toml_files(files: Mapping[str, dict] | None = None) -> Paths:
-        if files is None:
-            files = {"test.toml": {"s": {"k": "v"}}}
+    default = {"test.toml": {"s": {"k": "v"}}}
+    return functools.partial(gen_files, gen_toml_file, tmp_path, default)
 
-        file_paths = []
 
-        for name, config in files.items():
-            file_path = tmp_path / name
-            with open(file_path, "w") as f:
-                toml.dump(config, f)
-            file_paths.append(file_path)
-
-        return Paths(tmp_path, file_paths)
-
-    return gen_toml_files
+def gen_text_file(file_path: Path, content: str):
+    file_path.write_text(content)
 
 
 @pytest.fixture
 def text_files(tmp_path):
     """Create plain text files for testing."""
 
-    def gen_text_files(files: Mapping[str, str] | None = None):
-        if files is None:
-            files = {"test.txt": "Hello World"}
+    default = {"test.txt": "Hello World"}
+    return functools.partial(gen_files, gen_text_file, tmp_path, default)
 
-        file_paths = []
 
-        for name, text in files.items():
-            file_path = tmp_path / name
-            file_path.write_text(text)
-            file_paths.append(file_path)
-
-        return Paths(tmp_path, file_paths)
-
-    return gen_text_files
+def gen_json_file(file_path: Path, content: dict):
+    with open(file_path, "w") as f:
+        json.dump(content, f)
 
 
 @pytest.fixture
 def json_files(tmp_path):
     """Create JSON files for testing."""
 
-    def gen_json_files(files: Mapping[str, dict] | None = None):
-        if files is None:
-            files = {"test.json": {"key": "val"}}
-
-        file_paths = []
-
-        for name, config in files.items():
-            file_path = tmp_path / name
-            with open(file_path, "w") as f:
-                json.dump(config, f)
-            file_paths.append(file_path)
-
-        return Paths(tmp_path, file_paths)
-
-    return gen_json_files
+    default = {"test.json": {"k": "v"}}
+    return functools.partial(gen_files, gen_json_file, tmp_path, default)
