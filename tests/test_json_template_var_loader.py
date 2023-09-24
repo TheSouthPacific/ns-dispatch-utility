5a21f5e5ea34bb54438051558be44524221d835e
watarichanno <watarichanno@gmail.com>
2023-09-25 00:34:52 +0700

    Rename tests

--- tests/test_json_template_var_loader.py
+++ tests/test_json_template_var_loader.py
@@ -4,7 +4,7 @@ from nsdu import loader_api
 from nsdu.loaders import json_template_var_loader
 
 
-class TestJsonTemplateVarLoader:
+class TestGetTemplateVars:
     @pytest.mark.parametrize(
         "vars,expected",
         [
@@ -12,7 +12,7 @@ class TestJsonTemplateVarLoader:
             [{"t1.json": {"k": "v1"}, "t2.json": {"k": "v2"}}, {"k": "v2"}],
         ],
     )
-    def test_get_template_vars_returns_json_content_dict(
+    def test_with_valid_json_files_returns_json_content_dict(
         self, json_files, vars, expected
     ):
         paths = json_files(vars).file_paths
@@ -22,22 +22,22 @@ class TestJsonTemplateVarLoader:
 
         assert result == expected
 
-    def test_get_template_vars_from_invalid_json_raises_exception(self, text_files):
+    def test_with_no_json_file_returns_empty_dict(self):
+        config = {"json_template_var_loader": {"template_var_paths": []}}
+
+        result = json_template_var_loader.get_template_vars(config)
+
+        assert result == {}
+
+    def test_with_invalid_json_file_raises_exception(self, text_files):
         path = text_files({"t.json": "{"}).file_paths[0]
         config = {"json_template_var_loader": {"template_var_paths": [path]}}
 
         with pytest.raises(loader_api.LoaderError):
             json_template_var_loader.get_template_vars(config)
 
-    def test_get_template_vars_from_non_existent_file_raises_exception(self):
-        loader_config = {"json_template_var_loader": {"template_var_paths": ["a"]}}
+    def test_with_non_existent_file_raises_exception(self):
+        config = {"json_template_var_loader": {"template_var_paths": ["a"]}}
 
         with pytest.raises(loader_api.LoaderError):
-            json_template_var_loader.get_template_vars(loader_config)
-
-    def test_no_template_var_path_provided_returns_empty_dict(self):
-        loader_config = {"json_template_var_loader": {"template_var_paths": []}}
-
-        result = json_template_var_loader.get_template_vars(loader_config)
-
-        assert result == {}
+            json_template_var_loader.get_template_vars(config)
