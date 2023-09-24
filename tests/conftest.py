c59bca26d6574cdcf2b6920ef4b9afc99d7a158e
watarichanno <watarichanno@gmail.com>
2023-09-24 21:40:07 +0700

    Return temp folder and file paths

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,7 +13,7 @@ class Paths(NamedTuple):
 
 @pytest.fixture
 def toml_files(tmp_path):
-    """Generate TOML config files for testing."""
+    """Generate TOML files for testing."""
 
     def gen_toml_files(files: Mapping[str, dict] | None = None) -> Paths:
         if files is None:
@@ -53,15 +53,18 @@ def text_files(tmp_path):
 def json_files(tmp_path):
     """Create JSON files for testing."""
 
-    def gen_json_files(files={"test.json": {"key": "value"}}):
-        f_path = None
-        for name, content in files.items():
-            f_path = tmp_path / name
-            with open(f_path, "w") as f:
-                json.dump(content, f)
+    def gen_json_files(files: Mapping[str, dict] | None = None):
+        if files is None:
+            files = {"test.json": {"key": "val"}}
 
-        if len(files) == 1:
-            return f_path
-        return tmp_path
+        file_paths = []
+
+        for name, config in files.items():
+            file_path = tmp_path / name
+            with open(file_path, "w") as f:
+                json.dump(config, f)
+            file_paths.append(file_path)
+
+        return Paths(tmp_path, file_paths)
 
     return gen_json_files
